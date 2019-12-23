90fe01055c868b823a370d7b01ff115a5674b4c3
Henry Paulino <henrypl360@gmail.com>
2019-12-23 13:50:44 +0000

    update Container.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,18 +1,26 @@
 // @flow
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import { BLACK } from 'utils/colors'
+import { View } from 'react-native'
 
 type ContainerProps = {
   children: React$Node,
   style?: View.propTypes.style,
+  justifyContent?: 'center' | 'flex-start' | 'flex-end' | 'space-between',
+  alignItems?: 'center' | 'flex-start' | 'flex-end',
 }
 
 export default function Container(props: ContainerProps): React$Node {
+  const {
+    children,
+    style,
+    justifyContent = 'flex-start',
+    alignItems = 'flex-start',
+  } = props
   const defaultStyles = {
     paddingHorizontal: 20,
+    justifyContent,
+    alignItems,
   }
-  const { children, style } = props
 
   return <View style={[defaultStyles, style]}>{children}</View>
 }
