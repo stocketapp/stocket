012aeecf6a7b12fccd8f1937346a91020129c0f6
Henry Paulino <henrypl360@gmail.com>
2019-12-23 10:54:41 +0000

    style changes

--- src/components/PortfolioList/PortfolioEmpty.js
+++ src/components/PortfolioList/PortfolioEmpty.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { View, TouchableOpacity } from 'react-native'
 import Text from '../Text'
+import { BLUE } from 'utils/colors'
+
+const PortfolioEmpty = () => (
+  <View style={styles.container}>
+    <Text cap>Portfolio is empty</Text>
+
+    <TouchableOpacity style={styles.btn}>
+      <Text>Start Trading</Text>
+    </TouchableOpacity>
+  </View>
+)
 
 const styles = {
   container: {
@@ -9,14 +20,13 @@ const styles = {
     height: 150,
     width: '100%',
   },
+  btn: {
+    backgroundColor: BLUE,
+    paddingVertical: 8,
+    paddingHorizontal: 18,
+    borderRadius: 100,
+    marginTop: 20,
+  },
 }
-
-const PortfolioEmpty = () => (
-  <View style={styles.container}>
-    <Text cap>Portfolio is empty</Text>
-
-    
-  </View>
-)
 
 export default PortfolioEmpty
