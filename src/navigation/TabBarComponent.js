096d82395acc643f14ea15f9f5c1d891f90053d2
Henry Paulino <henrypl360@gmail.com>
2020-01-02 10:46:56 +0000

    add tab bar component

---
+++ src/navigation/TabBarComponent.js
@@ -0,0 +1,59 @@
+// @flow
+import React from 'react'
+import { TouchableOpacity, Dimensions, StyleSheet } from 'react-native'
+import { Container } from 'components'
+import { GRAY_DARKER } from 'utils/colors'
+
+type TabBarProps = {
+  renderIcon: ({
+    route: {},
+    tintColor: string,
+    focused: boolean,
+  }) => React$Node,
+  navigation: {
+    navigate: (route: string) => void,
+    state: { routes: Array<any>, index: number },
+  },
+}
+
+export default function TabBarComponent(props: TabBarProps): React$Node {
+  const { renderIcon, navigation } = props
+  const routes: Array<any> = navigation.state?.routes
+  const routesLength: number = routes.length
+  const tabWidth: number = Dimensions.get('screen').width / routesLength
+  const activeRoute: number = navigation.state?.index
+
+  return (
+    <Container style={styles.container} horizontal>
+      {routes.map((route, routeIndex) => {
+        const focused: boolean = activeRoute === routeIndex
+        const tintColor: string = focused ? '#fff' : GRAY_DARKER
+
+        return (
+          <TouchableOpacity
+            onPress={() => navigation.navigate(route.key)}
+            key={route.key}
+            style={{
+              width: tabWidth,
+              alignItems: 'center',
+              paddingVertical: 10,
+            }}
+          >
+            {renderIcon({ route, tintColor, focused })}
+          </TouchableOpacity>
+        )
+      })}
+    </Container>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#242424',
+    paddingBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: { height: -5, width: 0 },
+    shadowRadius: 3,
+    shadowOpacity: 0.1,
+  },
+})
--- src/navigation/mainStack.js
+++ src/navigation/mainStack.js
@@ -1,15 +1,26 @@
-import { createStackNavigator } from 'react-navigation-stack'
+import React from 'react'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 import Home from 'views/Home'
 import Trade from 'views/Trade'
-import { BLACK } from 'utils/colors'
+import { BLACK, GRAY_DARKER } from 'utils/colors'
+import TabBarComponent from './TabBarComponent'
+import { TradeIcon, HomeIcon } from 'icons'
 
-export default createStackNavigator(
+export default createBottomTabNavigator(
   {
     Home: {
       screen: Home,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => <HomeIcon size={34} color={tintColor} />,
+      },
     },
     Trade: {
       screen: Trade,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <TradeIcon size={34} color={tintColor} />
+        ),
+      },
     },
   },
   {
@@ -19,6 +30,15 @@ export default createStackNavigator(
     },
     defaultNavigationOptions: {
       header: null,
+    },
+    tabBarComponent: TabBarComponent,
+    tabBarOptions: {
+      activeTintColor: '#fff',
+      inactiveTintColor: GRAY_DARKER,
+      keyboardHidesTabBar: true,
+      style: {
+        paddingVertical: 10,
+      },
     },
   },
 )
