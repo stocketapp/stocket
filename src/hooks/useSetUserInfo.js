ecdfadb5d25e1eac96ad8141abc825fdf019eaed
Henry Paulino <henrypl360@gmail.com>
2020-02-01 19:46:48 -0500

    fix set user info

--- src/hooks/useSetUserInfo.js
+++ src/hooks/useSetUserInfo.js
@@ -4,20 +4,23 @@ import { useDispatch, useSelector } from 'react-redux'
 
 const UsersRef = firestore().collection('Users')
 
-export default function useSetUserInfo() {
+export default function useSetUserInfo(currentUser) {
   const [loading, setLoading] = useState(false)
   const dispatch = useDispatch()
-  const { userInfo, currentUser } = useSelector(({ user }) => user)
+  const { userInfo } = useSelector(({ user }) => user)
 
   useEffect(() => {
     async function getUserInfo() {
       try {
         setLoading(true)
         const info = await UsersRef.doc(currentUser?.uid).get()
-        dispatch({
-          type: 'SET_USER_INFO',
-          userInfo: info.data(),
-        })
+        const data = info.data()
+        if (data) {
+          dispatch({
+            type: 'SET_USER_INFO',
+            userInfo: data,
+          })
+        }
       } catch (err) {
         console.log(err)
       } finally {
@@ -26,7 +29,7 @@ export default function useSetUserInfo() {
     }
 
     getUserInfo()
-  }, [currentUser])
+  }, [currentUser, dispatch])
 
   return { loading, userInfo }
 }
