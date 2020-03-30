04e3e1080e2bd2a8fcae2b9329dcb153c001c9df
Henry Paulino <henrypl360@gmail.com>
2020-03-30 16:53:19 -0400

    open and render articles

--- src/views/Stock/StockNews.js
+++ src/views/Stock/StockNews.js
@@ -1,33 +1,51 @@
-import React from 'react'
-import { View, Image } from 'react-native'
-import { Text } from 'components'
+import React, { useRef, forwardRef } from 'react'
+import { View, Image, TouchableOpacity } from 'react-native'
+import { Text, ModalWebview } from 'components'
 import { GRAY_DARKER } from 'utils/colors'
 
-const ArticleItem = ({ article }) => (
-  <View style={styles.articleContainer}>
-    <View>
-      <Image source={{ uri: article?.image }} style={styles.img} />
-    </View>
-    <View style={{ paddingLeft: 20, flex: 1, justifyContent: 'space-between' }}>
-      <Text type="title" weight="500" numberOfLines={3} ellipsizeMode="tail">
-        {article?.headline}
+const ArticleItem = ({ article }) => {
+  const ref = useRef()
+
+  return (
+    <>
+      <TouchableOpacity onPress={() => ref.current.open()}>
+        <View style={styles.articleContainer}>
+          <View>
+            <Image source={{ uri: article?.image }} style={styles.img} />
+          </View>
+          <View
+            style={{ paddingLeft: 20, flex: 1, justifyContent: 'space-between' }}
+          >
+            <Text
+              type="title"
+              weight="500"
+              numberOfLines={3}
+              ellipsizeMode="tail"
+            >
+              {article?.headline}
+            </Text>
+            <Text color={GRAY_DARKER}>{article?.source}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <ModalWebview ref={ref} uri={article?.url} />
+    </>
+  )
+}
+
+export default function StockNews({ articles }) {
+  return (
+    <View style={styles.container}>
+      <Text type="heading" weight="900" style={{ paddingBottom: 10 }}>
+        News
       </Text>
-      <Text color={GRAY_DARKER}>{article?.source}</Text>
+
+      {articles.map((article, i) => (
+        <ArticleItem article={article} key={i} />
+      ))}
     </View>
-  </View>
-)
-
-export default ({ articles }) => (
-  <View style={styles.container}>
-    <Text type="heading" weight="900" style={{ paddingBottom: 10 }}>
-      News
-    </Text>
-
-    {articles.map((article, i) => (
-      <ArticleItem article={article} key={i} />
-    ))}
-  </View>
-)
+  )
+}
 
 const styles = {
   container: {
