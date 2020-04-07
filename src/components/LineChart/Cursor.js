a152ed599b0a0a4d926073ddfa51c369f73486c3
Henry Paulino <henrypl360@gmail.com>
2020-04-07 02:37:34 -0400

    fix bug with cursor position

---
+++ src/components/LineChart/Cursor.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Dimensions } from 'react-native'
+import { getPointAtLength, parsePath } from 'react-native-redash'
+import { PanGestureHandler, State } from 'react-native-gesture-handler'
+import Animated, {
+  event,
+  interpolate,
+  sub,
+  decay,
+} from 'react-native-reanimated'
+import { GREEN, SUB_BACKGROUND } from 'utils/colors'
+
+const { Value } = Animated
+const { width } = Dimensions.get('window')
+
+export default ({ d }) => {
+  const translationX = new Value(0)
+  const velocityX = new Value(0)
+  const state = new Value(State.UNDETERMINED)
+  const onGestureEvent = event([
+    {
+      nativeEvent: {
+        x: translationX,
+        velocityX,
+        state,
+      },
+    },
+  ])
+  const cx = decay(translationX, state, velocityX)
+  const path = parsePath(d)
+  const length = interpolate(translationX, {
+    inputRange: [0, width],
+    outputRange: [0, path.totalLength],
+  })
+  const { x, y } = getPointAtLength(path, length)
+  const translateX = sub(x, 5)
+  const cursorX = sub(x, 8)
+  const cursorY = sub(y, 4)
+
+  return (
+    <PanGestureHandler
+      onGestureEvent={onGestureEvent}
+      onHandlerStateChange={onGestureEvent}
+    >
+      <Animated.View>
+        <Animated.View style={[styles.line, { transform: [{ translateX }] }]} />
+        <Animated.View
+          style={[
+            styles.cursor,
+            { transform: [{ translateX: cursorX, translateY: cursorY }] },
+          ]}
+        />
+      </Animated.View>
+    </PanGestureHandler>
+  )
+}
+
+const styles = {
+  line: {
+    height: '100%',
+    width: 2,
+    backgroundColor: SUB_BACKGROUND,
+  },
+  cursor: {
+    height: 10,
+    width: 10,
+    borderRadius: 5,
+    backgroundColor: GREEN,
+    position: 'absolute',
+  },
+}
--- src/components/LineChart/index.js
+++ src/components/LineChart/index.js
@@ -1,18 +1,13 @@
-import React, { useState, useRef, useEffect } from 'react'
-import {
-  View,
-  StyleSheet,
-  Dimensions,
-  Animated,
-  PanResponder,
-} from 'react-native'
+import React, { useState, useRef } from 'react'
+import { View, StyleSheet, Dimensions, PanResponder } from 'react-native'
 import { Svg, Path, Line } from 'react-native-svg'
 import * as scale from 'd3-scale'
 import * as shape from 'd3-shape'
 import * as format from 'd3-format'
 import * as axis from 'd3-axis'
-import { scaleBand, scaleLinear, scaleTime } from 'd3-scale'
-import * as path from 'svg-path-properties'
+import { scaleLinear, scaleTime } from 'd3-scale'
+import Cursor from './Cursor'
+import { GREEN } from 'utils/colors'
 
 const d3 = {
   scale,
@@ -24,19 +19,19 @@ const height = 300
 const { width } = Dimensions.get('window')
 const verticalPadding = 30
 
-const data = [
-  { x: new Date(2020, 1, 1), y: 2100 },
-  { x: new Date(2020, 2, 1), y: 3100 },
-  { x: new Date(2020, 3, 1), y: 5100 },
-  { x: new Date(2020, 4, 1), y: 6100 },
-  { x: new Date(2020, 5, 1), y: 9100 },
-  { x: new Date(2020, 6, 1), y: 21000 },
-  { x: new Date(2020, 7, 1), y: 14000 },
-  { x: new Date(2020, 8, 1), y: 21000 },
-  { x: new Date(2020, 9, 1), y: 31000 },
-  { x: new Date(2020, 10, 1), y: 50100 },
-  { x: new Date(2020, 11, 1), y: 70100 },
-  { x: new Date(2020, 12, 1), y: 90100 },
+const arr = [
+  { at: new Date(2020, 1, 1), value: 2100 },
+  { at: new Date(2020, 2, 1), value: 3100 },
+  { at: new Date(2020, 3, 1), value: 5100 },
+  { at: new Date(2020, 4, 1), value: 6100 },
+  { at: new Date(2020, 5, 1), value: 9100 },
+  { at: new Date(2020, 6, 1), value: 21000 },
+  { at: new Date(2020, 7, 1), value: 14000 },
+  { at: new Date(2020, 8, 1), value: 21000 },
+  { at: new Date(2020, 9, 1), value: 31000 },
+  { at: new Date(2020, 10, 1), value: 50100 },
+  { at: new Date(2020, 11, 1), value: 70100 },
+  { at: new Date(2020, 12, 1), value: 90100 },
 ]
 
 const scaleX = scaleTime()
@@ -47,46 +42,21 @@ const scaleY = scaleLinear()
   .domain([0, 90100])
   .range([height - verticalPadding, verticalPadding])
 
-const line = d3.shape
-  .line()
-  .x(d => scaleX(d.x))
-  .y(d => scaleY(d.y))
-  .curve(d3.shape.curveBasis)(data)
-const lineProperties = path.svgPathProperties(line)
-const lineLength = lineProperties.getTotalLength()
-
-export default function LineChart() {
-  // const [positionX] = useState(new Animated.Value(0))
-  const [positionX, setPositionX] = useState(0)
-  // const [showLine, setShowLine] = useState(false)
-  const cursor = useRef()
-  const panResponder = useRef(
-    PanResponder.create({
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderGrant: (e, state) => {
-        setPositionX(state.moveX)
-        // setShowLine(true)
-      },
-      onPanResponderMove: (e, state) => {
-        setPositionX(state.moveX)
-      },
-    }),
-  ).current
+export default function LineChart({ data = arr }) {
+  const line = d3.shape
+    .line()
+    .x(d => scaleX(d.at))
+    .y(d => scaleY(d.value))
+    .curve(d3.shape.curveBasis)(arr)
 
   return (
-    <View style={styles.container} {...panResponder.panHandlers}>
+    <View style={styles.container}>
       <Svg {...{ width, height }}>
-        <Path d={line} fill="transparent" stroke="red" strokeWidth="2" />
-        <Line
-          x1={positionX}
-          y1="0"
-          x2={positionX}
-          y2={height}
-          stroke="red"
-          strokeWidth="1"
-        />
-        <View ref={cursor} style={styles.cursor} />
+        <Path d={line} fill="transparent" stroke={GREEN} strokeWidth="2" />
       </Svg>
+      <View style={{ ...StyleSheet.absoluteFill, width }}>
+        <Cursor d={line} />
+      </View>
     </View>
   )
 }
