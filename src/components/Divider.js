9461e8f37e90029059133d22fb9e61020d68158a
Henry Paulino <henrypl360@gmail.com>
2020-05-08 15:39:50 -0400

    create reusable divider component

---
+++ src/components/Divider.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View } from 'react-native'
+import { GRAY_DARKER } from 'utils/colors'
+
+type DividerPropTypes = {
+  vertical?: boolean,
+  width?: number,
+}
+
+export default ({ vertical = false, number = 0.5 }: DividerPropTypes) => {
+  const styles = {
+    width: vertical ? number : '100%',
+    height: vertical ? '100%' : number,
+    backgroundColor: GRAY_DARKER,
+    ...(vertical ? { marginHorizontal: 15 } : { marginVertical: 15 }),
+  }
+
+  return <View style={styles} />
+}
