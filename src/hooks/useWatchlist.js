6d8a5c49038c2715c4dcc049220e775185feaf72
Henry Paulino <henrypl360@gmail.com>
2020-06-21 15:18:49 -0400

    create useWatchlist  hook

---
+++ src/hooks/useWatchlist.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import firestore from '@react-native-firebase/firestore'
+import { useSelector } from 'react-redux'
+import { getBatchStockData } from 'api'
+import map from 'lodash.map'
+
+export default function useWatchlist() {
+  const { uid } = useSelector(({ user }) => user.currentUser)
+  const [watchlist, setWatchlist] = useState([])
+
+  useEffect(() => {
+    const subscribe = firestore()
+      .collection(`Users/${uid}/watchlist`)
+      .onSnapshot(async snapshot => {
+        const list = []
+        snapshot.docChanges().forEach(({ type, doc }) => {
+          if (type === 'added') {
+            list.push(doc.data().symbol)
+          }
+        })
+        const res = await getBatchStockData(list)
+        setWatchlist(map(res, el => el))
+      })
+
+    return () => subscribe()
+  }, [uid])
+
+  return watchlist
+}
