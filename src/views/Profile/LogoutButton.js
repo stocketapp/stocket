12a11c62209a5f7d086ee38c92a6e0a188576ae5
Henry Paulino <henrypl360@gmail.com>
2020-07-03 18:13:12 -0400

    Reset to home screen on logout

--- src/views/Profile/LogoutButton.js
+++ src/views/Profile/LogoutButton.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { TouchableOpacity } from 'react-native'
 import { Text } from 'components'
 import auth from '@react-native-firebase/auth'
+import { useNavigation } from '@react-navigation/native'
 
 export default function () {
+  const { navigate } = useNavigation()
+
   const signout = async () => {
+    navigate('Home')
     await auth().signOut()
   }
 
