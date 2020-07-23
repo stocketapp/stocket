0df35ae98af8a0ee7ec1b6a07daba2c68e61d670
Henry Paulino <henrypl360@gmail.com>
2020-07-23 16:22:38 -0400

    bundle version bump to v33

--- create_config.rb
+++ create_config.rb
@@ -6,6 +6,7 @@ IAPHUB_API_KEY = ENV['IAPHUB_API_KEY']
 IAPHUB_APPID = ENV['IAPHUB_APPID']
 IEX_URL = ENV['IEX_URL']
 IAPHUB_ENV = ENV['IAPHUB_ENV']
+APPSTORE_APP_SECRET = ENV['APPSTORE_APP_SECRET']
 
 open('config.js', 'w') { |output_file|
   output_file.puts "export const IEX_CLOUD_KEY = '#{IEX_CLOUD_KEY}'"
@@ -14,5 +15,6 @@ open('config.js', 'w') { |output_file|
   output_file.puts "export const IAPHUB_APPID = '#{IAPHUB_APPID}'"
   output_file.puts "export const IEX_URL = '#{IEX_URL}'"
   output_file.puts "export const IAPHUB_ENV = '#{IAPHUB_ENV}'"
+  output_file.puts "export const APPSTORE_APP_SECRET = '#{APPSTORE_APP_SECRET}'"
   # output_file.puts "export const variable3 = '#{path}'"
 }
