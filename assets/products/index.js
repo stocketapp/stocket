a0c9a70ce052b2f820adfffd13064c40b6586183
Henry Paulino <henrypl360@gmail.com>
2020-08-04 18:58:28 -0400

    update product images

--- assets/products/index.js
+++ assets/products/index.js
@@ -1,6 +1,6 @@
 export default {
   'com.corasan.stocket.5k_cash': require('./com.corasan.stocket.5k_cash.jpg'),
   'com.corasan.stocket.10k_cash': require('./com.corasan.stocket.10k_cash.jpg'),
-  'com.corasan.stocket.25k_cash': require('./com.corasan.stocket.25k_cash.jpg'),
+  'com.corasan.stocket.40k_cash': require('./com.corasan.stocket.40k_cash.jpg'),
   'com.corasan.stocket.75k_cash': require('./com.corasan.stocket.75k_cash.jpg'),
 }
