3a4c3594a031ac5ab28e84d2b57caa85309499cb
Henry Paulino <henrypl360@gmail.com>
2020-10-18 02:07:11 -0400

    update pckges, install svg transformer, add google url scheme

--- metro.config.js
+++ metro.config.js
@@ -4,15 +4,27 @@
  *
  * @format
  */
+const { getDefaultConfig } = require('metro-config')
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-        resetCache: true,
-      },
-    }),
-  },
-}
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig()
+
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+          resetCache: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  }
+})()
