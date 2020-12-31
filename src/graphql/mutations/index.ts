50af0e5fd3ed8894421c0f2dcb31043a210a3a64
Henry Paulino <henrypl360@gmail.com>
2020-12-31 11:47:14 -0500

    create refetch query hook

--- src/graphql/mutations/index.ts
+++ src/graphql/mutations/index.ts
@@ -1,13 +1,5 @@
 import useCreateUser from './useCreateUserMutation'
 import useAddToWatchlist from './useAddToWatchlist'
-import { FirebaseAuthTypes } from '@react-native-firebase/auth'
-import { FetchResult } from '@apollo/client'
-
-type Result = Promise<FetchResult<any, Record<string, any>, Record<string, any>>>
-interface StocketMutations {
-  useCreateUser: (user: FirebaseAuthTypes.User) => Result
-  addToWatchlist: (symbol: string) => Result
-}
 
 const stocketMutations = {
   useCreateUser,
