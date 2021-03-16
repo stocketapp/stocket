248b31ec13d25558f376457b4713fb7b787b2aa5
Henry Paulino <henrypl360@gmail.com>
2021-03-15 21:28:07 -0400

    use new jsx transform

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react-native/no-inline-styles': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
   },
 }
