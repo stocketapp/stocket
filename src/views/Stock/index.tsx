54a6e19bbb59b4e47fddf097a095947a4e5d43ad
Henry Paulino <henrypl360@gmail.com>
2021-03-22 17:14:02 -0400

    render price data on chart

--- src/views/Stock/index.tsx
+++ src/views/Stock/index.tsx
@@ -5,17 +5,18 @@ import { useQuery } from '@apollo/client'
 import { SYMBOL_QUOTE_QUERY, SYMBOL_CHART_QUERY } from './queries'
 import { IEXQuote } from 'types'
 import StockHeader from './StockHeader'
-import StockChart from './StockChart'
+import StockChart, { StockPriceChartItemType } from './StockChart'
 
 export default function Stock() {
   const { data } = useQuery(SYMBOL_QUOTE_QUERY, { variables: { symbol: 'AAPL' } })
   const { data: chartData } = useQuery(SYMBOL_CHART_QUERY, { variables: { symbol: 'AAPL' } })
 
   const quote: IEXQuote = data?.quote
+  const chart: StockPriceChartItemType[] = chartData?.chart
   return (
     <Container fullView safeAreaTop ph>
       <StockHeader {...quote} />
-      <StockChart data={chartData} />
+      <StockChart data={chart} />
     </Container>
   )
 }
