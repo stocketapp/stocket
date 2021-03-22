f4ed5a6450c67f42caeb492e0638fb979e9ac306
Henry Paulino <henrypl360@gmail.com>
2021-03-22 13:50:34 -0400

    minor style changes

--- src/views/Stock/StockeHeader.tsx
+++ src/views/Stock/StockeHeader.tsx
@@ -1,4 +1,4 @@
-import { Image } from 'react-native'
+import { Image, View } from 'react-native'
 import { Container, Text } from '@components'
 import { useTheme } from '@emotion/react'
 import { formatCurrency } from '@utils/functions'
@@ -6,29 +6,34 @@ import { IEXQuote } from 'types'
 
 const StockHeader = (props: IEXQuote) => {
   const { colors, p } = useTheme()
-  const { logo, symbol, change, latestPrice, changePercent } = props
+  const { logo, symbol, change, latestPrice, changePercent, companyName } = props
   const isPositive = change > 0
   const color = isPositive ? colors?.GREEN : change < 0 ? colors?.RED : 'white'
 
   return (
-    <Container top={p.lg}>
-      <Container horizontal alignItems="flex-end" justifyContent="flex-start">
-        <Image source={{ uri: logo }} style={{ width: 42, height: 42, borderRadius: 50 }} />
-        <Text type="big" weight="Black" pl={p.md} pt={2.5}>
-          {symbol}
-        </Text>
+    <Container>
+      <Container horizontal alignItems="center">
+        <Image source={{ uri: logo }} style={{ width: 50, height: 50, borderRadius: 50 }} />
+        <Container>
+          <Text type="heading" weight="Black" pl={p.lg}>
+            {companyName}
+          </Text>
+          <Text type="title" weight="Medium" pl={p.lg} color={colors.GRAY}>
+            {symbol}
+          </Text>
+        </Container>
       </Container>
-      <Container top={p.lg}>
-        <Text type="big" weight="Black">
-          {formatCurrency(Number(latestPrice?.toFixed(2)))}
-        </Text>
-      </Container>
-      <Container horizontal>
+      <Text type="big" weight="Black" pt={p.xlg}>
+        {formatCurrency(Number(latestPrice?.toFixed(2)))}
+      </Text>
+      <Container horizontal top={p.sm}>
         <Text type="title" weight="Heavy" color={color}>
+          {change > 0 && '+'}
           {change?.toFixed(2)}{' '}
         </Text>
-        <Text type="label" weight="Semibold" color={colors.GRAY}>
-          ({changePercent?.toFixed(2)}%)
+        <Text type="label" weight="Medium" color={colors.GRAY}>
+          ({change > 0 && '+'}
+          {changePercent?.toFixed(2)}%)
         </Text>
       </Container>
     </Container>
