e337dca153a4d6ba9a4a3a4e0334d001a4fe25b9
Henry Paulino <henrypl360@gmail.com>
2021-03-24 17:04:43 -0400

    create add to watchlist reusable button

--- src/views/Stock/index.tsx
+++ src/views/Stock/index.tsx
@@ -1,15 +1,21 @@
-import { useEffect, useState } from 'react'
-import { Container } from '@components'
-import StockContentLoader from './StockContentLoader'
-import StockHeader from './StockHeader'
-import StockChart from './StockChart'
-import useStockHook from './hooks/useStockHooks'
-import { useRoute } from '@react-navigation/core'
+import { TouchableOpacity } from 'react-native'
 
 export default function Stock() {
+  const { useEffect, useState } = require('react')
+  const { Container, AddToWatchlistButton } = require('@components')
+  const { useRoute, useNavigation } = require('@react-navigation/core')
+  const { ArrowLeftIcon } = require('@icons')
+  const StockContentLoader = require('./StockContentLoader')
+  const StockHeader = require('./StockHeader').default
+  const StockChart = require('./StockChart').default
+  const useStockHook = require('./hooks/useStockHook').default
+  const { useTheme } = require('@emotion/react')
+
   const { params }: any = useRoute()
-  const { quote, chart } = useStockHook(params?.symbol)
+  const { quote, chart } = useStockHook(params && params?.symbol)
   const [renderChart, setRenderChart] = useState(false)
+  const { colors } = useTheme()
+  const { goBack } = useNavigation()
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -27,7 +33,14 @@ export default function Stock() {
   }
 
   return (
-    <Container fullView useNavBar scrollable>
+    <Container fullView scrollable>
+      <Container ph separate horizontal>
+        <TouchableOpacity style={{ paddingVertical: 5, paddingRight: 5 }} onPress={goBack}>
+          <ArrowLeftIcon size={34} color={colors.GREEN} />
+        </TouchableOpacity>
+        <AddToWatchlistButton symbol={params?.symbol} />
+      </Container>
+
       <Container ph top={15}>
         <StockHeader {...quote?.data} />
       </Container>
