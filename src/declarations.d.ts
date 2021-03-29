854f8962169ac4d9cd750391b6d34ef50b4ccb8d
Henry Paulino <henrypl360@gmail.com>
2021-03-29 00:29:59 -0400

    add arrow icons

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,7 +1,6 @@
 import { CustomTheme } from 'theme'
 
 declare module '*.svg' {
-  import * as React from 'react';
   import { SvgProps } from 'react-native-svg'
   const content: React.FC<SvgProps>
   export default content
