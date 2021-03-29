a35381e67bc1ab47a01c084e0dbbab35cc0f0442
Henry Paulino <henrypl360@gmail.com>
2021-03-29 00:32:49 -0400

    remove balance change text

--- src/components/Balance/index.tsx
+++ src/components/Balance/index.tsx
@@ -1,19 +1,18 @@
-import { GREEN, RED } from '@utils/colors'
+import { GREEN, RED, LABEL } from '@utils/colors'
 import Text from '../Text'
 import { formatCurrency } from '@utils/functions'
 import { PortfolioQueryType } from 'views/Home/hooks/useHomeHook'
 import { ChangeContainer, BalanceContaienr, valueStyle } from './styles'
+import { ArrowUpIcon, ArrowDownIcon } from '@icons'
 
 const Balance = ({ value, change, changePct }: PortfolioQueryType): JSX.Element => {
-  const color = changePct > 0 ? GREEN : changePct < 0 ? RED : 'white'
-  const upOrDown = changePct > 0 ? 'up' : 'down'
+  const isPositive = changePct > 0
+  const color = isPositive ? GREEN : changePct < 0 ? RED : 'white'
+  const Arrow = isPositive ? ArrowUpIcon : ArrowDownIcon
   const formattedChange = (change ?? 0).toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
   })
-  const day = new Date().getDay()
-  const isWeekend = day === 6 ? 'yesterday' : day === 0 ? 'on Friday' : 'today'
-  const is = day === 6 || day === 0 ? 'was' : 'is'
 
   return (
     <BalanceContaienr>
@@ -21,20 +20,10 @@ const Balance = ({ value, change, changePct }: PortfolioQueryType): JSX.Element 
         {formatCurrency(value)}{' '}
       </Text>
       <ChangeContainer>
-        <Text weight="Medium" type="label">
-          Your portfolio {is} {upOrDown}{' '}
+        <Arrow size={20} color={color} />
+        <Text weight="Medium" type="label" color={LABEL}>
           <Text weight="Bold" color={color} type="label">
-            {(changePct ?? 0)?.toFixed(2)}%{' '}
-          </Text>
-          {isWeekend},
-        </Text>
-        <Text color={color} weight="Bold" type="label">
-          <Text weight="Medium" type="label">
-            it gained{' '}
-          </Text>
-          {formattedChange}{' '}
-          <Text weight="Medium" type="label">
-            in total value.
+            {formattedChange} ({(changePct ?? 0)?.toFixed(2)}% )
           </Text>
         </Text>
       </ChangeContainer>
