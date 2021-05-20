fe16006a109767af344c17b1732d0a2446db7672
Henry Paulino <henrypl360@gmail.com>
2021-05-20 14:10:28 -0400

    query the user and save it in the cache

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,11 +1,13 @@
 import { InMemoryCache, makeVar, ReactiveVar } from '@apollo/client'
 import { WatchlistIexQuote } from 'views/Home/Watchlist/WatchlistItem'
+import { UserType } from 'types'
 
 export const watchlistSymbolsVar: ReactiveVar<string[]> = makeVar<string[]>([])
 export const isWatchlistLoadingVar = makeVar(true)
 export const watchlistQuotesVar: ReactiveVar<WatchlistIexQuote[]> = makeVar<any[]>([])
 export const portfolioValueVar: ReactiveVar<any> = makeVar<any>(null)
 export const isPortfolioLoadingVar: ReactiveVar<any> = makeVar<any>(null)
+export const userVar: ReactiveVar<UserType | null> = makeVar<UserType | null>(null)
 
 const cache = new InMemoryCache({
   typePolicies: {
