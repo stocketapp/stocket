634bdf04a915c5a31b1bdb900d75786e7f92b51e
Henry Paulino <henrypl360@gmail.com>
2021-05-29 14:26:10 -0400

    style changes to trade modal

--- src/views/Stock/StockTradeModal/index.tsx
+++ src/views/Stock/StockTradeModal/index.tsx
@@ -1,55 +1,27 @@
-import { Dispatch, SetStateAction, useMemo, useEffect } from 'react'
+import { Dispatch, SetStateAction, useMemo } from 'react'
 import { Modal, TouchableWithoutFeedback } from 'react-native'
-import {
-  TradeContentContainer,
-  ButtonTrade,
-  quantityContainer,
-  HStack,
-  totalContainerStyles,
-} from './styles'
+import { TradeContentContainer, quantityContainer, HStack } from './styles'
 import { IEXQuote } from 'types'
 import { VStack, purchaseDetails } from './styles'
-import { Text, VirtualNumPad } from '@components'
+import { Text } from '@components'
 import { useState } from 'react'
-import { useTheme } from '@emotion/react'
-import Company from './Company'
-import { formatCurrency } from '@utils/functions'
+import TradeModalHeader from './TradeModalHeader'
 import { userVar } from '@cache'
 import { useReactiveVar } from '@apollo/client'
+import TradeModalKeyboard from './TradeModalKeyboard'
 
 function StockTradeModal({ quote, visible, setVisible }: StockTradeModalProps) {
   const [quantity, setQuantity] = useState('0')
-  const theme = useTheme()
   const user = useReactiveVar(userVar)
-  const total = useMemo(() => Number(quantity) * quote?.latestPrice, [
-    quantity,
-    quote?.latestPrice,
-  ])
+
   const cash = user?.cash || 0
   const maxShares = useMemo(() => (cash / quote?.latestPrice).toFixed(2), [
     cash,
     quote?.latestPrice,
   ])
 
-  useEffect(() => {
-    quantity === '' && setQuantity('0')
-  }, [quantity])
-
-  const numPadRemove = () => {
-    setQuantity(() => quantity.slice(0, -1))
-  }
-
-  const onNumPadPress = (str: string) => {
-    if (quantity === '' && str === '.') {
-      setQuantity('0.')
-    } else {
-      setQuantity(() => {
-        if (str === '.' && quantity.includes('.')) {
-          return quantity
-        }
-        return quantity.concat(str).replace(/^0/g, '')
-      })
-    }
+  const onKeyPress = (value: string) => {
+    setQuantity(value)
   }
 
   return (
@@ -62,7 +34,7 @@ function StockTradeModal({ quote, visible, setVisible }: StockTradeModalProps) {
     >
       <TouchableWithoutFeedback onPressOut={() => setVisible(false)}>
         <TradeContentContainer>
-          <Company
+          <TradeModalHeader
             name={quote?.companyName}
             logo={quote?.logo}
             price={quote?.latestPrice}
@@ -70,29 +42,16 @@ function StockTradeModal({ quote, visible, setVisible }: StockTradeModalProps) {
 
           <VStack style={purchaseDetails}>
             <HStack style={quantityContainer}>
-              <Text type="huge">{quantity}</Text>
+              <Text type="huge">{(Number(quantity) || 0).toLocaleString('en-US')}</Text>
             </HStack>
             <HStack style={quantityContainer}>
               <Text type="subtext" color="LIGHT_GRAY" weight="Semibold">
                 Max: {maxShares}
               </Text>
             </HStack>
-
-            <HStack style={totalContainerStyles}>
-              <Text type="title" weight="Medium" color="LIGHT_GRAY">
-                Total
-              </Text>
-              <Text type="heading" weight="Bold">
-                {formatCurrency(total || 0)}
-              </Text>
-            </HStack>
-            <ButtonTrade style={{ backgroundColor: theme.colors.GREEN }}>
-              <Text type="heading" weight="Bold">
-                Buy
-              </Text>
-            </ButtonTrade>
           </VStack>
-          <VirtualNumPad onKeyPress={onNumPadPress} onDelete={numPadRemove} />
+
+          <TradeModalKeyboard onKeyPress={onKeyPress} />
         </TradeContentContainer>
       </TouchableWithoutFeedback>
     </Modal>
