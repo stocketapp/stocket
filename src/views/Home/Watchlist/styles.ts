c6996e3dc827077b4ddcd3fbbf86a48c0c277c50
Henry Paulino <henrypl360@gmail.com>
2021-06-01 18:29:52 -0400

    update and cleanup WatchlistItem styles

--- src/views/Home/Watchlist/styles.ts
+++ src/views/Home/Watchlist/styles.ts
@@ -10,29 +10,6 @@ export const ImageContainer = styled.View(({ theme }) => ({
   paddingRight: theme.p.md,
 }))
 
-export const ItemLeftContainer = styled.View({
-  flex: 0.8,
-  flexDirection: 'row',
-})
-
-export const ItemRightContainer = styled.View({
-  justifyContent: 'space-between',
-  alignItems: 'flex-end',
-})
-
-export const WatchlistItemContainer = styled.View(({ theme }) => ({
-  paddingVertical: theme.p.lg,
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  width: '100%',
-}))
-
-export const SymbolAndName = styled.View(({ theme }) => ({
-  justifyContent: 'space-between',
-  paddingLeft: theme.p.md,
-}))
-
 export const Change = styled.View(({ theme }) => ({
   paddingVertical: theme.p.xsm,
   paddingHorizontal: theme.p.sm,
