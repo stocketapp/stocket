bda3d8916be4b4ec981b3cc60d8bbf46f331b486
Henry Paulino <henrypl360@gmail.com>
2021-07-03 18:25:46 -0400

    refetch at tabview root instead of position tab

--- src/views/Stock/StockPositionTab/index.tsx
+++ src/views/Stock/StockPositionTab/index.tsx
@@ -1,31 +1,7 @@
 import { Container, Text } from '@components'
-import { GET_POSITION } from '../queries'
-import { useQuery } from '@apollo/client'
-import { RouteProp, useRoute } from '@react-navigation/native'
-import { StockStackParamsList } from 'navigation/stacks/StockStack'
-import { useFocusEffect } from '@react-navigation/core'
-import { useCallback } from 'react'
 import { PositionType } from 'types'
 
-export default function StockPositionTab({ activeTab }: StockPositionTabProps) {
-  const { params } = useRoute<RouteProp<StockStackParamsList, 'Stock'>>()
-  const { data, refetch } = useQuery(GET_POSITION, {
-    variables: { symbol: params?.symbol },
-  })
-  const position = data?.position
-
-  useFocusEffect(
-    useCallback(() => {
-      const refetchInterval = setInterval(async () => {
-        if (activeTab === 1) {
-          await refetch()
-        }
-      }, 15000)
-
-      return () => clearInterval(refetchInterval)
-    }, [activeTab, refetch]),
-  )
-
+export default function StockPositionTab({ position }: StockPositionTabProps) {
   return (
     <Container fullView>
       <Text>{position?.totalValue}</Text>
