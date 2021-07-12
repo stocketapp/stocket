ac91356c28a9e9fa4ee9c3fcf9ab6448324457d8
Henry Paulino <henrypl360@gmail.com>
2021-07-12 19:43:16 -0400

    add details to position tab

--- src/views/Stock/StockPositionTab/index.tsx
+++ src/views/Stock/StockPositionTab/index.tsx
@@ -1,10 +1,35 @@
 import { Container, Text } from '@components'
 import { PositionType } from 'types'
+import { PositionDetailContainer } from './styles'
+import { formatCurrency } from '@utils/functions'
+
+const PositionDetail = ({ label, value }: DetailProps) => (
+  <PositionDetailContainer>
+    <Text type="label" weight="Medium" color="GRAY">
+      {label}
+    </Text>
+    <Text type="heading" weight="Heavy">
+      {value}
+    </Text>
+  </PositionDetailContainer>
+)
+
+interface DetailProps {
+  label: string
+  value: string | number
+}
 
 export default function StockPositionTab({ position }: StockPositionTabProps) {
   return (
-    <Container fullView>
-      <Text>{position?.totalValue}</Text>
+    <Container fullView ph items="center">
+      <Container content="center" horizontal>
+        <PositionDetail label="Shares" value={position?.positionSize} />
+        <PositionDetail label="Total" value={formatCurrency(position?.totalValue)} />
+      </Container>
+      <Container content="center" horizontal>
+        <PositionDetail label="Change" value={formatCurrency(position?.change)} />
+        <PositionDetail label="Change %" value={position?.changePct} />
+      </Container>
     </Container>
   )
 }
