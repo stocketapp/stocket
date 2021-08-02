9b892acd668bd1c43443f82934850bb19569846b
Henry Paulino <henrypl360@gmail.com>
2021-08-02 12:04:27 -0400

    rename and move position tab component

--- src/views/Stock/Tabs/Position/index.tsx
+++ src/views/Stock/Tabs/Position/index.tsx
@@ -1,23 +1,7 @@
-import { Container, Text } from '@components'
+import { Container } from '@components'
 import { PositionType } from 'types'
-import { PositionDetailContainer } from './styles'
 import { formatCurrency } from '@utils/functions'
-
-const PositionDetail = ({ label, value }: DetailProps) => (
-  <PositionDetailContainer>
-    <Text type="label" weight="Medium" color="GRAY">
-      {label}
-    </Text>
-    <Text type="heading" weight="Heavy">
-      {value}
-    </Text>
-  </PositionDetailContainer>
-)
-
-interface DetailProps {
-  label: string
-  value: string | number
-}
+import PositionDetail from './PositionDetail'
 
 export default function StockPositionTab({ position }: StockPositionTabProps) {
   return (
