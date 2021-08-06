50609d82bf1ca3a63e20d4eacc189dfc8c666fc3
Henry Paulino <henrypl360@gmail.com>
2021-08-06 13:16:02 -0400

    update theme

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,5 @@
-import { CustomTheme } from './theme'
+import { StocketTheme } from './theme'
 
 declare module '@emotion/react' {
-  export interface Theme extends CustomTheme {}
+  export interface Theme extends StocketTheme {}
 }
