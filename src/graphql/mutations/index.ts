f136492efe684c415c7168b394b986d6baf6f4a2
Henry Paulino <henrypl360@gmail.com>
2021-08-09 18:31:27 -0400

    update to AddToWatchlist and useStocketMutation.ts

--- src/graphql/mutations/index.ts
+++ src/graphql/mutations/index.ts
@@ -1,5 +1,4 @@
 import useCreateUserMutation from './useCreateUserMutation'
-import { gql } from '@apollo/client'
 
 export default function useStocketMutation() {
   const createUser = useCreateUserMutation()
@@ -7,26 +6,3 @@ export default function useStocketMutation() {
     createUser,
   }
 }
-
-export const ADD_TO_WATCHLIST = gql`
-  mutation ($input: AddToWatchlistInput!) {
-    addToWatchlist(input: $input) {
-      symbol
-      symbol
-      change
-      changePercent
-      companyName
-      latestPrice
-      id
-      logo
-    }
-  }
-`
-
-export const REMOVE_FROM_WATCHLIST = gql`
-  mutation RemoveFromWatchlist($input: RemoveFromWatchlistInput!) {
-    removeFromWatchlist(input: $input) {
-      symbol
-    }
-  }
-`
--- src/hooks/useStocketMutation.ts
+++ src/hooks/useStocketMutation.ts
@@ -6,11 +6,11 @@ type mutationType = DocumentNode | TypedDocumentNode<any, Record<string, any>>
 type mutationOptions = MutationHookOptions<any, Record<string, any>> | undefined
 type variablesType = Record<string, any> | undefined
 
-export default function useStocketMutation(
+export default function useStocketMutation<StocketDataType>(
   mutation: mutationType,
   options?: mutationOptions,
 ) {
-  const [mutate] = useMutation(mutation, options)
+  const [mutate] = useMutation<StocketDataType>(mutation, options)
 
   const callback = useCallback(
     (input: variablesType, update?: MutationUpdaterFn) =>
