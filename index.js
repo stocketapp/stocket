343cde5ac2adba86ddefc039ec2ce017c7785ddf
Henry Paulino <henrypl360@gmail.com>
2021-08-24 09:17:59 -0400

    install sentry

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ import AsyncStorage from '@react-native-community/async-storage'
 import { ApolloProvider } from '@apollo/client'
 import Reactotron from 'reactotron-react-native'
 import { ThemeProvider } from '@emotion/react'
-const store = configureStore()
 import { useFlipper } from '@react-navigation/devtools'
+import * as Sentry from '@sentry/react-native'
 import App from './App'
 import { name as appName } from './app.json'
 import configureStore from './src/redux/configureStore'
 import client from './src/ApolloClient'
 import theme from './src/theme'
 
+const store = configureStore()
 if (global.HermesInternal) {
   // Hide timers warning (Android only)
   LogBox.ignoreLogs(['Setting a timer'])
@@ -36,12 +37,50 @@ if (__DEV__) {
   Reactotron.setAsyncStorageHandler(AsyncStorage).configure().useReactNative().connect()
 }
 
+const routingInstrumentation = new Sentry.ReactNavigationV5Instrumentation()
+if (!__DEV__) {
+  Sentry.init({
+    dsn: 'https://0e632d3ccb63458f98b7e70784b7819c@o563230.ingest.sentry.io/5741924',
+    tracesSampleRate: 0.2,
+    environment: __DEV__ ? 'development' : 'production',
+    integrations: [
+      new Sentry.ReactNativeTracing({
+        tracingOrigins: [
+          'localhost',
+          'api.stocketapp.com',
+          'api.stocketapp.com/graphql',
+          /^\//,
+        ],
+        routingInstrumentation,
+        beforeNavigate: context => {
+          if (context.data.route.name === 'Do Not Send') {
+            context.sampled = false
+          }
+          context.name = context.name.toUpperCase()
+          context.tags = {
+            ...context.tags,
+            customTag: 'value',
+          }
+          return context
+        },
+      }),
+    ],
+  })
+}
+
 const AppRoot = () => {
   const navigationRef = useNavigationContainerRef()
   useFlipper(navigationRef)
 
   return (
-    <NavigationContainer ref={navigationRef}>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        if (!__DEV__) {
+          routingInstrumentation.registerNavigationContainer(navigationRef)
+        }
+      }}
+    >
       <ApolloProvider client={client}>
         <Provider store={store}>
           <SafeAreaProvider>
