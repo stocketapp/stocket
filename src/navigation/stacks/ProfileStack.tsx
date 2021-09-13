395cc35c14019e47fc28be05dcaa81a1cbad6d8f
Henry Paulino <henrypl360@gmail.com>
2021-09-13 00:01:10 -0400

    update stack

--- src/navigation/stacks/ProfileStack.tsx
+++ src/navigation/stacks/ProfileStack.tsx
@@ -2,11 +2,13 @@ import {
   createNativeStackNavigator,
   NativeStackNavigationProp,
 } from '@react-navigation/native-stack'
-import { Profile, AddCash } from '@views'
+import { Profile, AddCash, PurchasesHistory, TradesHistory } from '@views'
 
 export type ProfileStackParamsList = {
   Profile: object | undefined
   AddCash: object | undefined
+  PurchasesHistory: object | undefined
+  TradesHistory: object | undefined
 }
 
 export type ProfileNavigationProps = NativeStackNavigationProp<ProfileStackParamsList>
@@ -23,6 +25,8 @@ export default function StockStack() {
       <Screen name="Profile" component={Profile} />
       <Group screenOptions={{ presentation: 'modal' }}>
         <Screen name="AddCash" component={AddCash} />
+        <Screen name="PurchasesHistory" component={PurchasesHistory} />
+        <Screen name="TradesHistory" component={TradesHistory} />
       </Group>
     </Navigator>
   )
